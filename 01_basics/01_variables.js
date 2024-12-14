ed24db6428d23cf0730de3e6147baea8e69aa08c
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-14 14:37:55 +0000

    new_file creation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 153462
+let accountEmail = "deveshjangde2006@gmail.com"
+var accountPasswart = 511115
+accountInfo = "karhi"
+// accountId = 32 // not allowed
+
+accountEmail = "yogeshjangde2006@gmail.com"
+accountPasswart = 1111111
+accountInfo = "mungeli"
+
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountPasswart,accountInfo,accountState]);
+
+/*
+prefer not to use var
+because of issue  in block scope and functional scope
+*/
+
