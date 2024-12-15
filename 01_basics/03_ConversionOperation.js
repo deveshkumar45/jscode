6923432230501ec010314ffc08e7c6695f6f69ef
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-15 14:54:07 +0000

    Operation being continue

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -25,7 +25,36 @@ let booleanIsLogedIn = Boolean(isLogedIn)
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+ // ************************************** Operations *********************************************
+
+  let value = 3
+  let negValue = -value
+  // console.log(negValue);
+   
+//   console.log(2+2);
+//   console.log(2-2);
+//   console.log(2*2);
+//   console.log(2**3);
+//   console.log(2/2);
+//   console.log(2%4);
+
+let str1 = "devesh "
+let str2 = "jangde"
+let stre2 = str1 + str2;
+
+// console.log(stre2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "3");
 
 
+
+
+
+
+  
