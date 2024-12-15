4dd87595ced09d0dcd0fdde238f66b557c838303
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-15 14:28:11 +0000

    Converstion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = "devesh"
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber = Number (score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLogedIn = "devesh"
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+//  console.log(booleanIsLogedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "devesh" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
