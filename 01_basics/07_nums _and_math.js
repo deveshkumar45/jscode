6aae61db01bd95b1bbafebfd40688ae1be680b7e
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-19 05:32:17 +0000

    nums and maths

---
+++ 01_basics/07_nums _and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber =  23.426265
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor (4.5));
+// console.log(Math.max(3,5,6,3,2,1));
+// console.log(Math.min(3,2,1,4,5,6));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random()* (max - min +1 )) + min)
+
+
+
+
+
+
+
+
+
+
