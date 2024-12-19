482c975c54643c464d80a81028d9aed25e5e6824
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-19 16:39:46 +0000

    datainJs

---
+++ 01_basics/08_datainJs.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatDate = new Date(2023,0, 18)
+// let myCreatDate = new Date(2023,0, 18 , 4, 2)
+let myCreatDate = new Date("2-14-2024")
+// console.log(myCreatDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
