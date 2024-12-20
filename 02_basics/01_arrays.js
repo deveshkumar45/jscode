373090de6a1fcd8ae3ab9ef536dc07513ab76586
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-20 16:37:38 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myArr = [0,1,2,3,4,5,8,9]
+const myHeros = ["shaktiman", "krish" , "Ra-one"]
+
+const myArr2 = new Array(1,3,43,5,54)
+
+// Array methods
+
+// myArr.push(23)
+// myArr.push(12)
+// myArr.pop()
+
+// myArr.unshift(99)
+// myArr.shift()
+
+
+// console.log(myArr.includes(88));
+// console.log(myArr.indexOf(44));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+// slice & splice 
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
