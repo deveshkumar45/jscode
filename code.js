cff7d766d7a26cfc8fd3d25f9b0d01304b44d822
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-26 19:07:03 +0000

    string finish

--- code.js
+++ code.js
@@ -24,8 +24,8 @@ const newIs = "mango, papaya, apple, orange"
 // ++++++
 // const isLess = newIs.substring(7,20)
 // const Is = newIs.length
-const isLess = newIs.substr(-12)
-console.log(isLess);
+const isLess = newIs.substr(5,11)
+// console.log(isLess);
 
 // Converting to Upper and Lower Case:
 
@@ -51,4 +51,45 @@ const leg = 5;
 const leg1 = leg.toString();
 // console.log(leg1.padEnd(6,4))
 
+// String repeat()
 
+let text = "Hello Devesh "
+let result = text.repeat(2)
+// console.log(result);
+
+
+// Replacing String Content
+
+let text2 = "i could speak english"
+//let result1 = text2.replace(/ENGLISH/i, "hindi"); //  use a regular expression with an /i flag (insensitive):
+let result1 = text2.replace("english","Hindi");
+// console.log(result1);
+
+// use a regular expression with a /g flag (global match):
+
+const ren = "Please visit Microsoft & Microsoft !"
+// const rell = ren.replace(/Microsoft/g, "Ws3cube");
+const rell = ren.replaceAll("Microsoft", "Ws3cube");
+// console.log(rell);
+
+// JavaScript String split()
+const spell = "devesh abhinay yogesh dushyant chandni shivani"
+const myArr = spell.split(" "); // commas, spaces, pipe
+// console.log(myArr);
+
+// let text4 = `We are the so-called "Vikings" from the north.`;
+// let text4 = "We are the so-called \"Vikings\" from the north.";
+// let text4= 'It\'s alright.';
+let text4 = "The character \\ is called backslash.";
+// console.log(text4);
+
+let nameIs = "devesh"
+let nameIs1 = new String("devesh")
+// console.log( typeof nameIs1);
+
+//JavaScript String includes()
+
+let strong = "devesh and aman was best friends."
+// console.log(strong.includes("devesh"));
+let textile = "Hello world, welcome to the universe.";
+console.log(textile.startsWith("Hel"));
