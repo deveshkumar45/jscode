3a53784f3741e45f13b61bf015edd569fbffb97b
Devesh Kumar <deveshjangde2006@gmail.com>
2024-12-29 14:41:48 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,5 +20,50 @@ const regularUser = {
 }
 // console.log(regularUser.fullname.userFullName.firstname);
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
 
-const obj1 = {}
+// const obj3 = { obj1,obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "D@gmail.com"
+    },
+    {
+        id: 2,
+        email: "A@gmail.com"
+    },
+    {
+        id: 3,
+        email: "Y@gmail.com"
+    }
+]
+
+// console.log(users[1].email);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("isLogged")); 
+
+const course = {
+    coursename: "js in hindi",
+    price: 999,
+    courseInstructor: "Hitesh choudhary"
+}
+
+const {courseInstructor: instructor} = course // de-structuring 
+console.log(instructor);
+
+
+// {
+//     "name": "Devesh",
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
