fe8f67d18cb12f7af4862a6fcbb5398594dc6bb0
Devesh Kumar <deveshjangde2006@gmail.com>
2025-02-12 18:22:59 +0000

    third change 3

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,10 +2,10 @@ const marvel_heros = ["Thor", "captain", "vision", "Iron man", "spider-man"]
 const dc_heros = ["Batman", "superman", "flash"]
 
 // marvel_heros.push(dc_heros)
-// console.log(marvel_heros[5][1]);
+// console.log(marvel_heros[5][2]);
 
 const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+console.log(allHeros);
  
 const all_new_heros = [...marvel_heros, ...dc_heros] // spread 
 // console.log(all_new_heros);
