eade1722f5634ac03f6a3eec1fd323ffd702c0ed
Devesh Kumar <deveshjangde2006@gmail.com>
2025-02-12 18:22:46 +0000

    second change2

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,27 +2,32 @@
 
 const myArr = [0,1,2,3,4,5,8,9]
 const myHeros = ["shaktiman", "krish" , "Ra-one"]
-
+const mySuperHeros = ["Super-man","Batman","Flash","wonderwooman","Aquaman"]
 const myArr2 = new Array(1,3,43,5,54)
+
+// console.log(myArr[4]);
+// console.log(myHeros.push(10))
 
 // Array methods
 
-// myArr.push(23)
-// myArr.push(12)
-// myArr.pop()
-
-// myArr.unshift(99)
-// myArr.shift()
+// console.log(myArr.push(12));
+myArr.push(12)
+myArr.pop()
 
 
-// console.log(myArr.includes(1));
+myArr.unshift(99)
+myArr.shift()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(6)); 
 // console.log(myArr.indexOf(1));
 // console.log( typeof myArr);
 
-const newArr = myArr.join("_") //  returns an array as a string:
-
+const newArr = myArr.join("-") //  returns an array as a string:
+const newArr1 = myArr.toLocaleString()
 // console.log(myArr);
-// console.log( typeof newArr);
+console.log(  newArr1);
 
 // slice & splice 
 
